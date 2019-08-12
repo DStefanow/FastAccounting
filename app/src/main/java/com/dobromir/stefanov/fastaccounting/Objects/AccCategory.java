65b570fe79f42bf1443f055f6a3b90af54176ff5
mopak <dobromirstefanov96@gmail.com>
2019-08-12 23:07:39 +0300

    Add category object

---
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccCategory.java
@@ -0,0 +1,31 @@
+package com.dobromir.stefanov.fastaccounting.Objects;
+
+public class AccCategory {
+    private int id;
+    private String categoryName;
+    private int parent_category_id;
+    private int repeat_on;
+
+    public AccCategory(int id, String categoryName, int parent_category_id, int repeat_on) {
+        this.id = id;
+        this.categoryName = categoryName;
+        this.parent_category_id = parent_category_id;
+        this.repeat_on = repeat_on;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public int getParent_category_id() {
+        return parent_category_id;
+    }
+
+    public int getRepeat_on() {
+        return repeat_on;
+    }
+}
