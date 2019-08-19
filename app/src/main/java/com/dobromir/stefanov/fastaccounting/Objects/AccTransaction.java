1f409b3cfffbe04b27a0a323ab1fb8546218523f
mopak <dobromirstefanov96@gmail.com>
2019-08-19 22:10:58 +0300

    Add datetime column

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccTransaction.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccTransaction.java
@@ -1,11 +1,14 @@
 package com.dobromir.stefanov.fastaccounting.Objects;
 
+import java.time.LocalDateTime;
+
 public class AccTransaction {
     private int id;
     private AccountingType accType;
     private AccCategory accCategory;
     private double amount;
     private String note;
+    private LocalDateTime transactionTime;
 
     public AccTransaction(int id, AccountingType accType, AccCategory accCategory, double amount, String note) {
         this.id = id;
@@ -33,5 +36,9 @@ public class AccTransaction {
 
     public String getNote() {
         return note;
+    }
+
+    public LocalDateTime getDateOn() {
+        return transactionTime;
     }
 }
