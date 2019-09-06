210ab23b7c02af18fc2b98e88589b3c6cfa1057a
mopak <dobromirstefanov96@gmail.com>
2019-09-06 18:46:39 +0300

    Prepare object for db entity

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccCategory.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccCategory.java
@@ -1,36 +1,78 @@
 package com.dobromir.stefanov.fastaccounting.Objects;
 
-public class AccCategory {
-    private int id;
-    private String categoryName;
-    private int parent_category_id;
-    private int repeat_on;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
 
-    public AccCategory(int id, String categoryName, int parent_category_id, int repeat_on) {
-        this.id = id;
-        this.categoryName = categoryName;
-        this.parent_category_id = parent_category_id;
-        this.repeat_on = repeat_on;
+@Entity(tableName = "categories",
+    indices = {@Index(value = {"category_name"}, unique = true)})
+public class AccCategory {
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name = "category_name")
+    private String categoryName;
+
+    @ColumnInfo(name = "parent_category_id")
+    private int parentCategoryId;
+
+    @ColumnInfo(name = "repeat_on")
+    private int repeatOn;
+
+    public AccCategory(int id, String categoryName, int parentCategoryId, int repeatOn) {
+        setId(id);
+        setCategoryName(categoryName);
+        setParentCategoryId(parentCategoryId);
+        setRepeatOn(repeatOn);
     }
 
-
     public AccCategory(String categoryName) {
-        this.categoryName = categoryName;
+        setCategoryName(categoryName);
     }
 
     public int getId() {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getCategoryName() {
         return categoryName;
     }
 
-    public int getParent_category_id() {
-        return parent_category_id;
+    public void setCategoryName(String categoryName) {
+        if (categoryName.length() < 3) {
+            return;
+        }
+
+        this.categoryName = categoryName;
     }
 
-    public int getRepeat_on() {
-        return repeat_on;
+    public int getParentCategoryId() {
+        return parentCategoryId;
+    }
+
+    public void setParentCategoryId(int parentCategoryId) {
+        // We have already set parent category for this one
+        if (this.parentCategoryId != 0) {
+            return;
+        }
+
+        this.parentCategoryId = parentCategoryId;
+    }
+
+    public int getRepeatOn() {
+        return repeatOn;
+    }
+
+    public void setRepeatOn(int repeatOn) {
+        if (repeatOn < 1 ||repeatOn > 28) {
+            return;
+        }
+
+        this.repeatOn  = repeatOn;
     }
 }
