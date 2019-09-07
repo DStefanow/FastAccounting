0ffdad4b138a1a9c21289d545c47f6758c8ecb2a
mopak <dobromirstefanov96@gmail.com>
2019-09-07 23:11:50 +0300

    Convert AccTransaction in Room Entity object

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccTransaction.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccTransaction.java
@@ -1,44 +1,96 @@
 package com.dobromir.stefanov.fastaccounting.Objects;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
 import java.time.LocalDateTime;
 
+@Entity(tableName = "transactions",
+    foreignKeys = @ForeignKey(entity = AccCategory.class,
+        parentColumns = "id",
+        childColumns = "category_id",
+        onDelete = ForeignKey.CASCADE))
 public class AccTransaction {
+    @PrimaryKey
     private int id;
+
+    @ColumnInfo(name = "acc_type")
     private AccountingType accType;
-    private AccCategory accCategory;
+
+    @ColumnInfo(name = "category_id")
+    private int categoryId;
+
     private double amount;
+
     private String note;
+
+    @ColumnInfo(name = "transaction_time")
     private LocalDateTime transactionTime;
 
-    public AccTransaction(int id, AccountingType accType, AccCategory accCategory, double amount, String note) {
-        this.id = id;
-        this.accType = accType;
-        this.accCategory = accCategory;
-        this.amount = amount;
-        this.note = note;
+    public AccTransaction(int id, AccountingType accType, int categoryId, double amount, String note, LocalDateTime transactionTime) {
+        setId(id);
+        setAccType(accType);
+        setCategoryId(categoryId);
+        setAmount(amount);
+        setNote(note);
+        setTransactionTime(transactionTime);
     }
 
     public int getId() {
-        return id;
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public AccountingType getAccType() {
-        return accType;
+        return this.accType;
     }
 
-    public AccCategory getAccCategory() {
-        return accCategory;
+    public void setAccType(AccountingType accType) {
+        this.accType = accType;
+    }
+
+    public int getCategoryId() {
+        return this.categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
     }
 
     public double getAmount() {
-        return amount;
+        return this.amount;
+    }
+
+    public void setAmount(double amount) {
+        if (amount < 0.0) {
+            return;
+        }
+
+        this.amount = amount;
     }
 
     public String getNote() {
-        return note;
+        return this.note;
     }
 
-    public LocalDateTime getDateOn() {
-        return transactionTime;
+    public void setNote(String note) {
+        if (note.length() < 5) {
+            return;
+        }
+
+        this.note = note;
+    }
+
+    public LocalDateTime getTransactionTime() {
+        return this.transactionTime;
+    }
+
+    public void setTransactionTime(LocalDateTime transactionTime) {
+        this.transactionTime = transactionTime;
     }
 }
