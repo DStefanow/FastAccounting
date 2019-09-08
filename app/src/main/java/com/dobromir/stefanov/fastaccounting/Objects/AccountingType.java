db570df897ba2e4097ad91c159e25ae1aa2d344c
mopak <dobromirstefanov96@gmail.com>
2019-09-09 00:03:55 +0300

    Add codes to enum values

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccountingType.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/Objects/AccountingType.java
@@ -2,5 +2,16 @@ package com.dobromir.stefanov.fastaccounting.Objects;
 
 // Define the basic accounting types
 public enum AccountingType {
-    INCOME, OUTCOME
+    INCOME(0),
+    OUTCOME(1);
+
+    private int accTypeCode;
+
+    AccountingType(int accTypeCode) {
+        this.accTypeCode = accTypeCode;
+    }
+
+    public int getAccTypeCode() {
+        return accTypeCode;
+    }
 }
