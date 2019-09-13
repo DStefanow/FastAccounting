364701015bd4e96007d911d5119963c4e9592d26
mopak <dobromirstefanov96@gmail.com>
2019-09-13 18:06:59 +0300

    Init account transaction dao

---
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccTransactionDao.java
@@ -0,0 +1,31 @@
+package com.dobromir.stefanov.fastaccounting.DAOs;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.dobromir.stefanov.fastaccounting.Objects.AccTransaction;
+
+import java.util.List;
+
+@Dao
+public interface AccTransactionDao {
+    @Query("SELECT * FROM transactions WHERE acc_type = :accType")
+    List<AccTransaction> getTransactionsByType(int accType);
+
+    @Query("SELECT * FROM transactions WHERE category_id = :category_id")
+    List<AccTransaction> getTransactionsByCateogry(int category_id);
+
+    @Query("SELECT * FROM transactions WHERE amount BETWEEN :minAmount AND :maxAmount")
+    List<AccTransaction> getTransactionsBetweenAmount(double minAmount, double maxAmount);
+
+    @Query("SELECT * FROM transactions WHERE transaction_time BETWEEN :minDate AND :maxDate")
+    List<AccTransaction> getTransactionsBetweenTime(int minDate, int maxDate);
+
+    @Insert
+    void insertNewTransaction(AccTransaction accTransaction);
+
+    @Delete
+    void deleteTransaction(AccTransaction accTransaction);
+}
