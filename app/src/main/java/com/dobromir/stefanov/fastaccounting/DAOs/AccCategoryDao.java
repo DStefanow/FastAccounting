34d96305d1a868cebfdfc188df99b9589923e1b0
mopak <dobromirstefanov96@gmail.com>
2019-09-14 00:18:56 +0300

    Wrap select operations in LiveData to enable dynamic update

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccCategoryDao.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccCategoryDao.java
@@ -1,5 +1,7 @@
 package com.dobromir.stefanov.fastaccounting.DAOs;
 
+import android.arch.lifecycle.LiveData;
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -12,10 +14,10 @@ import java.util.List;
 @Dao
 public interface AccCategoryDao {
     @Query("SELECT * FROM categories")
-    List<AccCategory> getAllCategories();
+    LiveData<List<AccCategory>> getAllCategories();
 
     @Query("SELECT * FROM categories WHERE parent_category_id IS NULL")
-    List<AccCategory> getAllMainCategories();
+    LiveData<List<AccCategory>> getAllMainCategories();
 
     @Insert
     void insertNewAccCategory(AccCategory accCategory);
--- app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccTransactionDao.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccTransactionDao.java
@@ -1,5 +1,7 @@
 package com.dobromir.stefanov.fastaccounting.DAOs;
 
+import android.arch.lifecycle.LiveData;
+
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -12,16 +14,16 @@ import java.util.List;
 @Dao
 public interface AccTransactionDao {
     @Query("SELECT * FROM transactions WHERE acc_type = :accType")
-    List<AccTransaction> getTransactionsByType(int accType);
+    LiveData<List<AccTransaction>> getTransactionsByType(int accType);
 
     @Query("SELECT * FROM transactions WHERE category_id = :category_id")
-    List<AccTransaction> getTransactionsByCateogry(int category_id);
+    LiveData<List<AccTransaction>> getTransactionsByCateogry(int category_id);
 
     @Query("SELECT * FROM transactions WHERE amount BETWEEN :minAmount AND :maxAmount")
-    List<AccTransaction> getTransactionsBetweenAmount(double minAmount, double maxAmount);
+    LiveData<List<AccTransaction>> getTransactionsBetweenAmount(double minAmount, double maxAmount);
 
     @Query("SELECT * FROM transactions WHERE transaction_time BETWEEN :minDate AND :maxDate")
-    List<AccTransaction> getTransactionsBetweenTime(int minDate, int maxDate);
+    LiveData<List<AccTransaction>> getTransactionsBetweenTime(int minDate, int maxDate);
 
     @Insert
     void insertNewTransaction(AccTransaction accTransaction);
