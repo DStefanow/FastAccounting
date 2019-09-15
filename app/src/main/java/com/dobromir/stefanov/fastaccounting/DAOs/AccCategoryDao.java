b1fb987c904e1d5621b5dfee58de3ac249d9649c
mopak <dobromirstefanov96@gmail.com>
2019-09-15 15:50:11 +0300

    Comment for now delete method for acc category

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccCategoryDao.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/DAOs/AccCategoryDao.java
@@ -22,6 +22,6 @@ public interface AccCategoryDao {
     @Insert
     void insertNewAccCategory(AccCategory accCategory);
 
-    @Delete
-    void deleteAccCategory(AccCategory accCategory);
+    //@Delete
+    //void deleteAccCategory(AccCategory accCategory);
 }
