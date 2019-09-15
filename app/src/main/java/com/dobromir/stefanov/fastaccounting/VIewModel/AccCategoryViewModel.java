163c2f51b4e78438f31dcd6d69f92aa969564573
mopak <dobromirstefanov96@gmail.com>
2019-09-15 16:20:08 +0300

    Init ViewModel for Account Category

---
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/VIewModel/AccCategoryViewModel.java
@@ -0,0 +1,35 @@
+package com.dobromir.stefanov.fastaccounting.VIewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.dobromir.stefanov.fastaccounting.DAOs.AccCategoryRepository;
+import com.dobromir.stefanov.fastaccounting.Objects.AccCategory;
+
+import java.util.List;
+
+public class AccCategoryViewModel extends AndroidViewModel {
+    private AccCategoryRepository accCategoryRepository;
+    private LiveData<List<AccCategory>> allCategories;
+    private LiveData<List<AccCategory>> allMainCategories;
+
+    public AccCategoryViewModel(@NonNull Application application) {
+        super(application);
+        accCategoryRepository = new AccCategoryRepository(application);
+    }
+
+    public LiveData<List<AccCategory>> getAllCategories() {
+        return this.allCategories;
+    }
+
+    public LiveData<List<AccCategory>> getAllMainCategories() {
+        return this.allMainCategories;
+    }
+
+    public void insertCategory(AccCategory accCategory) {
+        accCategoryRepository.insertCategory(accCategory);
+    }
+}
