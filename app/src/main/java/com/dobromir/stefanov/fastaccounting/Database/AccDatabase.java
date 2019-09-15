6c692ad4a3b69b99e47642d0daf84745ef45ea31
mopak <dobromirstefanov96@gmail.com>
2019-09-15 23:46:07 +0300

    Add method that add simple categories in the database

--- app/src/main/java/com/dobromir/stefanov/fastaccounting/Database/AccDatabase.java
+++ app/src/main/java/com/dobromir/stefanov/fastaccounting/Database/AccDatabase.java
@@ -1,11 +1,15 @@
 package com.dobromir.stefanov.fastaccounting.Database;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import com.dobromir.stefanov.fastaccounting.Converters.LocalDateConverter;
 import com.dobromir.stefanov.fastaccounting.Converters.AccTypeConverter;
@@ -23,12 +27,37 @@ public abstract class AccDatabase extends RoomDatabase {
 
     private static volatile AccDatabase INSTANCE;
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDbAsync(INSTANCE).execute();
+        }
+    };
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private final AccCategoryDao mAccCategoryDao;
+
+        PopulateDbAsync(AccDatabase db) {
+            mAccCategoryDao = db.accCategoryDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mAccCategoryDao.insertNewAccCategory(new AccCategory("Food", 0));
+            mAccCategoryDao.insertNewAccCategory(new AccCategory("Car", 0));
+            return null;
+        }
+    }
+
     public static AccDatabase getDatabase(final Context context) {
         if (INSTANCE == null) {
             synchronized (AccDatabase.class) {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                            AccDatabase.class, "fast_accounting").build();
+                            AccDatabase.class, "fast_accounting").
+                            addCallback(sRoomDatabaseCallback).
+                            build();
                 }
             }
         }
